ecba120fe44681dcb91b68ce495b0e020fc58c08
sayuuuu <83942705+sayuuuu@users.noreply.github.com>
2021-05-10 15:01:01 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -21,13 +21,13 @@ async def help(event):
         else:
             await event.edit("**Module doesn't exist or Module name is invalid**")
     else:
-        await event.edit(f"**All modules are listed below**\
+        await event.edit(f"** Modules-: **\
             \nUsage: Type `.help <module name>` to know how it works\
             \nModules loaded: {len(modules)}")
         string = ""
         for i in sorted(CMD_HELP):
             string += "`" + str(i)
-            string += "`  -  "
+            string += "`  •  "
         modules_list = await event.reply(string)
         await asyncio.sleep(300)
         await modules_list.delete()
